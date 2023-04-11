28738604e143af9fb5aa5a72faf972fa0b466369
NAJIB-B <najibajibasu@gmail.com>
2023-04-11 17:28:29 +0100

    added navigation

--- components/signIn/signIn.jsx
+++ components/signIn/signIn.jsx
@@ -5,9 +5,48 @@ import Button from "../button/button";
 import ForgotPassword from "../forgotPassword/forgotPassword";
 import { useState } from "react";
 import Overlay from "../overlay/overlay";
+import { useRouter } from "next/router";
 import ResetPassword from "../resetPassword/resetPassword";
 
+const defaultFormFields={
+   password:"",   
+   email:"",
+}
+
 const SignIn = () => {
+   const router = useRouter()
+   const [formFields, setFormFields]= useState(defaultFormFields)
+    const {password, email} = formFields
+
+    const [emailError, setEmailError] = useState(false)
+    const [passwordError, setPasswordError] = useState(false)
+
+    const validate =()=>{
+      if (
+         !new RegExp(
+           /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+         ).test(email)
+       ) {
+        setEmailError(true)
+   
+         return
+       } else {
+         setEmailError(false)
+       }
+      if (!password.trim()) {
+         setPasswordError(true)
+         return
+       } else {
+         setPasswordError(false)
+       }
+       router.push("/dashboard")
+    }
+
+    const change =(e)=>{
+     const {name, value} = e.target;
+     setFormFields({...formFields, [name]:value})
+    }
+
    const [overlay, setOverlay] = useState(false)
    const closeForgotPassword =()=>{
       setOverlay(false)
@@ -28,15 +67,27 @@ const SignIn = () => {
                <input 
                className={Styles.input}
                type="text" 
-               placeholder="Email"/>
+               placeholder="Email"
+               name="email"
+               value={email}
+               onChange={change}/>
             </div>
+            {emailError ? (
+            <p className={Styles.error_message}> Please input a valid email</p>
+          ) : null}
             <div className={Styles.input_div}> 
                <FaLock size={25}></FaLock>
                <input 
                className={Styles.input}
+               name="password"
                type="text" 
-               placeholder="Password"/>
+               placeholder="Password"
+               value={password}
+               onChange={change}/>
             </div>
+            {passwordError ? (
+            <p className={Styles.error_message}> Please input your password</p>
+          ) : null}
             <div className={Styles.input_checkbox}> 
                <input type="checkbox"></input>
                 <p>Remember me</p>
@@ -49,9 +100,11 @@ const SignIn = () => {
             label={"Log in"}
             border={"none"}
             radius={"0.5rem"}
+            padding={"0.5rem"}
             marginTop={"2rem"}
             fontWeight={"600"}
-            fontSize={"1.1rem"}>
+            fontSize={"1.1rem"}
+            action={validate}>
             </Button>
 
             <p onClick={()=>{setOverlay(true)}} className={Styles.forgot_password_text}>Forgot your password?</p>
