9153d848fcef08956166e2d74b09a19ac84ba783
NAJIB-B <najibajibasu@gmail.com>
2023-06-21 18:16:39 +0100

    fixed padding in dasboard leftside

--- redux/dashboard.js
+++ redux/dashboard.js
@@ -1,21 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { dashboardMenu } from "@/utils/dashboard";
 
-const initialState ={
-selectedDashboardMenu : dashboardMenu.home,
-
-}
+const initialState = {
+  selectedDashboardMenu: dashboardMenu.home,
+};
 
 export const dashboardSlice = createSlice({
-    name:"dashboard",
-    initialState,
-    reducers:{
-        setSelectedDashboardMenu: (state, action) =>{
-          state.selectedDashboardMenu = action.payload
-        }
-    }
-})
+  name: "dashboard",
+  initialState,
+  reducers: {
+    setSelectedDashboardMenu: (state, action) => {
+      state.selectedDashboardMenu = action.payload;
+    },
+  },
+});
 
-export const {setSelectedDashboardMenu} = dashboardSlice.actions
+export const { setSelectedDashboardMenu } = dashboardSlice.actions;
 
-export default dashboardSlice.reducer
+export default dashboardSlice.reducer;
